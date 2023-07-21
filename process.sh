01fd701e039e9d95f181dba97eb16010997d8b46
NagarajuSamboju <nagarajusamboju@gmail.com>
2023-07-21 13:44:13 +0530

    commit 3

--- process.sh
+++ process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Hello $user"
-read username $user
-echo " hey i'm  $user i will be telling you the current process"
+read username 
+echo " hey i'm  $username i will be telling you the current process"
 echo " current running process list"
 ps

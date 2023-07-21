555c06748b55013beffe3334f632c7429cdbaf1c
NagarajuSamboju <nagarajusamboju@gmail.com>
2023-07-21 13:53:15 +0530

    interactive first commit

---
+++ interactive.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+echo "What is your first name"
+read fname
+echo "welcome Mr.$fname can you please tell me your last name "
+read lname
+echo " Thanks $lname $fname. Have a nice Journey"
+
